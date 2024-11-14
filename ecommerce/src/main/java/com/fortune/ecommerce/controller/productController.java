49bf09640a605b5f95db14451a5d16b006c8bcb8
Your Name <kanyangif@gmail.com>
2024-11-14 22:23:33 +0300

    ecommerce using Spring boot

---
+++ ecommerce/src/main/java/com/fortune/ecommerce/controller/productController.java
@@ -0,0 +1,15 @@
+package com.fortune.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class productController {
+
+ @RequestMapping("/")
+public String greet(){
+    return "Hello world";
+}
+    
+}
